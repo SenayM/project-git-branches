696cc031f2c20482586020494d1f5f95e2f42e61
SenayM <mailtosenay@gmail.com>
2016-10-31 15:00:07 +0100

    my commit from dev

--- src/Main.java
+++ src/Main.java
@@ -3,6 +3,8 @@ public class Main {
 
 	public static void main(String[] args) {
 		System.out.println("This line i added form dev branch");
+		System.out.println("This line is added form dev branch");
+		System.out.println("This line i added form dev branch");
 	}
 
 }
