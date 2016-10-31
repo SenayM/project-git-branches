33a0f5d028921e34d56f3841f1ae08a6b6cb33ba
SenayM <mailtosenay@gmail.com>
2016-10-31 15:01:40 +0100

    my commit form master branch

--- src/Main.java
+++ src/Main.java
@@ -3,6 +3,7 @@ public class Main {
 
 	public static void main(String[] args) {
 		System.out.println("This line i added form dev branch");
+		System.out.println("This line i added form master branch");
 	}
 
 }
